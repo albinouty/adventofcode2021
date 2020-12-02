9e96ab2cef1f8a9cc7702804ba8cd8db85096a32
albinouty <rickutykanski@gmail.com>
2020-12-01 20:52:31 -0700

    day 1 pt 1

---
+++ adventofcode2020/src/main/kotlin/day1.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val sum = 2020
+    for(i in input.indices) {
+        for(x in i + 1 until input.size)
+            if(input[i] + input[x] == sum){
+                val a = input[i]
+                val b = input[x]
+                println("The two digits that add to $sum are $a and $b. Multiplied together they are " + a*b)
+            }
+    }
+}
