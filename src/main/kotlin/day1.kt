7caf7572821892b2aca935c273578d85ff34c2a1
albinouty <rickutykanski@gmail.com>
2020-12-01 21:25:12 -0700

    pt 2 solution (#1)

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,11 +1,29 @@
 fun main(args: Array<String>) {
     val sum = 2020
+
+    /**
+    Part 1
+     */
     for(i in input.indices) {
         for(x in i + 1 until input.size)
             if(input[i] + input[x] == sum){
                 val a = input[i]
                 val b = input[x]
                 println("The two digits that add to $sum are $a and $b. Multiplied together they are " + a*b)
-            }s
+            }
+    }
+
+    /**
+    Part 2
+     */
+    for(i in input.indices) {
+        for(x in i + 1 until input.size)
+            for(z in x + 1 until input.size)
+                if(input[i] + input[x] + input[z] == sum){
+                    val a = input[i]
+                    val b = input[x]
+                    val c = input[z]
+                    println("The three digits that add to $sum are $a, $b, and $c. Multiplied together they are " + a * b * c)
+                }
     }
 }
