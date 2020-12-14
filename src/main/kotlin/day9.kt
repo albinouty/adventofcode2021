aa3acbe46bb1714e7d3f287108b188ef70bdeba6
albinouty <rickutykanski@gmail.com>
2020-12-14 15:49:02 -0700

    close on day 9

--- src/main/kotlin/day9.kt
+++ src/main/kotlin/day9.kt
@@ -1,45 +1,34 @@
 import java.io.File
 
-var churn = 5
+var churn = 25
 fun main(args: Array<String>) {
     fun readFileAsLinesUsingUseLines(fileName: String): List<String> = File(fileName).useLines { it.toList() }
     val inputDayNine = readFileAsLinesUsingUseLines("src/main/resources/inputday9.txt").map { it.toLong() }
-    println(inputDayNine)
-//    for (i in inputDayNine) {
-//        val tmp = inputDayNine.subList(0, churn)
-//        val sums = mutableListOf<Long>()
-//        var iter = 0
-//        for (x in tmp) {
-//            if (x != tmp[iter]) {
-//                sums.add(x + tmp[iter]) //you aren't coming up with every combination here.
-//            }
-//        }
-//        if (!sums.contains(inputDayNine[churn])) {
-//            println("the first number to break the pattern is ${inputDayNine[churn]}")
-//            break
-//        }
-//        churn += churn
-//    }
-    val tmp = inputDayNine.subList(0, churn)
-//    println(tmp)
-//    val what = tmp.cumulate()
-//    var z = 0
-//    for(i in inputDayNine) {
-//        val tmp = inputDayNine.subList(z, churn)
-//        val sums = mutableListOf<Long>()
-//        var iter = 0
-//        for(x in 0 until tmp.size -1) {
-//            for(j in x + 1 until tmp.size) {
-//                sums.add(tmp[x] + tmp[j])
-//            }
-//        }
-//        iter += 1
-//        if(!sums.contains(inputDayNine[churn])) {
-//            println("the first number to break the pattern is ${inputDayNine[churn]}")
-//            break
-//        }
-//        z = churn
-//        churn += churn
-//        println(sums)
-//    }
+    fun getPreamble(iter: Int): List<Long> {
+        return inputDayNine.subList(iter, churn+iter)
+    }
+    val values = inputDayNine.subList(churn, inputDayNine.size)
+    println(values)
+
+    fun validValues(z: Int): List<Long> {
+        val preamble = getPreamble(z)
+        val sums = mutableListOf<Long>()
+        for (i in preamble) {
+            for (x in preamble) {
+                if (i != x) {
+                    sums.add(i + x)
+                }
+            }
+        }
+        return sums.toList()
+    }
+
+    var iterator = 0
+    var ans = 0.toLong()
+    for(i in values) {
+        if(validValues(iterator).contains(i)) {
+            iterator += 1
+        } else ans = i
+    }
+    println(ans)
 }
