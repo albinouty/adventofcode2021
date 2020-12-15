a965c33491265b6d7d20876d786474e0d189ae2e
albinouty <rickutykanski@gmail.com>
2020-12-15 14:54:01 -0700

    optimizing day 15

--- src/main/kotlin/day15.kt
+++ src/main/kotlin/day15.kt
@@ -1,29 +1,27 @@
-fun main(args: Array<String>) {
-    val input = mutableListOf(11, 0, 1, 10, 5, 19)
+import kotlin.system.measureTimeMillis
 
-    fun findOccurances(i: Int, s: Int): Int {
-        val fList = input.subList(0, s).mapIndexed { index, x ->
-            if (x == i) {
-                index
-            } else 0
+fun main(args: Array<String>) {
+    val input = mutableListOf(0,3,6)
+    val time = measureTimeMillis {
+        var iter = input.size
+        while (true) {
+            val sub = input.size - 1
+            val lastElement = input.last()
+            val container = input.subList(0, sub)
+            iter += if (!container.contains(lastElement)
+            ) {
+                input.add(0)
+                1
+            } else {
+                val last = container.indexOfLast { it == lastElement } + 1
+                input.add(iter - last)
+                1
+            }
+            if (iter == 2020) {
+                break
+            }
         }
-        return fList.max()!!
+        println("the answer is ${input[2019]}")
     }
-    var iter = input.size
-    while (true) {
-        val sub = input.size - 1
-        iter += if (!input.subList(0, sub)
-                .contains(input.last())) {
-            input.add(0)
-            1
-        } else {
-            val last = findOccurances(input.last(), sub) + 1
-            input.add( iter - last)
-            1
-        }
-        if (iter == 2020) {
-            break
-        }
-    }
-    println("The 2020th number is ${input[2019]}")
+    println(time/1000)
 }
