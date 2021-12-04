aebb3231a2e19cef156f1dd626e377e60789d7d4
Rick Utykanski <rick.utykanski@ADMITLEADLAUGH.workdayinternal.com>
2021-12-04 15:24:24 -0700

    day 2 part 2

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -20,11 +20,30 @@ fun main(args: Array<String>) {
                 else -> throw Exception("FATAL ERROR: Unrecognized command in file")
             }
         }
-        println("Horizontal position is $dist and depth is $depth. These values multiplied together is ${dist * depth}.")
+        println("Part 1: Horizontal position is $dist and depth is $depth. These values multiplied together is ${dist * depth}.")
 
         /**
         Part 2
          */
+        dist = 0
+        depth = 0
+        var aim = 0
+        for(i in day2input) {
+            when(i.first) {
+                "forward" -> {
+                    dist += i.second
+                    depth += aim * i.second
+                }
+                "down" -> {
+                    aim += i.second
+                }
+                "up" -> {
+                    aim -= i.second
+                }
+                else -> throw Exception("FATAL ERROR: Unrecognized command in file")
+            }
+        }
+        println("Part 2: Horizontal position is $dist and depth is $depth. These values multiplied together is ${dist * depth}.")
     }
     day2()
 }
