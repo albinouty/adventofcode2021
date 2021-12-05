a9dc4adae154694295a86314dc172192d57f0d44
Rick Utykanski <rick.utykanski@ADMITLEADLAUGH.workdayinternal.com>
2021-12-05 11:06:51 -0700

    Day 3 part 2

--- src/main/kotlin/day3.kt
+++ src/main/kotlin/day3.kt
@@ -9,6 +9,9 @@ fun main(args: Array<String>) {
         }
     }
     val day3input = readFileAsLinesUsingUseLines("src/main/resources/day3data.txt")
+    fun convert(bit: String): Int {
+        return Integer.parseInt(bit,2)
+    }
     fun day3() {
         fun bitCounter(data: List<List<Int>>, index: Int) : Pair<Int, Int> {
             var on = 0
@@ -33,7 +36,6 @@ fun main(args: Array<String>) {
                 epsilon.add(1)
             }
         }
-
         var stringGammaBit = ""
         gamma.map {
             stringGammaBit = "$stringGammaBit$it"
@@ -42,15 +44,48 @@ fun main(args: Array<String>) {
         epsilon.map {
             stringEpsilonBit = "$stringEpsilonBit$it"
         }
-
-        fun convert(bit: String): Int {
-            return Integer.parseInt(bit,2)
-        }
-
         val gammaFinal = convert(stringGammaBit)
         val epsilonFinal = convert(stringEpsilonBit)
         println("Part 1: gamma value is ${gammaFinal} and epsilon value is ${epsilonFinal}. Multiplied together these are ${epsilonFinal*gammaFinal}")
+    }
 
+    fun day3part2() {
+        fun mostSignificantBit(data: List<List<Int>>, index: Int) : Int {
+            var on = 0
+            var off = 0
+            for(i in data) {
+                if(i[index] == 1) {
+                    on += 1
+                } else off += 1
+            }
+            return if( on >= off) {
+                1
+            } else 0
+        }
+
+        fun reduce(data: List<List<Int>>, index: Int, useMostCommon: Boolean): String{
+            var varIndex = index
+            val sigBit = mostSignificantBit(data, index)
+            val newSet = data.filter {
+                if(useMostCommon) {
+                    it[index] == sigBit
+                } else it[index] != sigBit
+            }
+            var ans = ""
+            return if(newSet.size == 1) {
+                newSet[0].map {
+                    ans = "$ans$it"
+                }
+                ans
+            } else {
+                varIndex += 1
+                reduce(newSet, varIndex, useMostCommon)
+            }
+        }
+        val oxyAns = convert(reduce(day3input, 0, true))
+        val co2Ans = convert(reduce(day3input, 0, false))
+        println("Part 2: The Oxygen Rating is $oxyAns and the CO2 Rating is $co2Ans. The product of these numbers is ${oxyAns*co2Ans}")
     }
     day3()
+    day3part2()
 }
